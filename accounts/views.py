877df8d290f9cf4d2a644b16ce6ccfa518bd1240
Hima Bindu <bindu.python@gmail.com>
2024-03-16 10:38:40 +0530

    handled cart and checkout

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,9 @@ from accounts.models import Account
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
 
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
@@ -61,13 +64,63 @@ def login(request):
         user = auth.authenticate(email=email,password=password) #it wil return user object
         
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request)) #if cart items are their inside the cart  it run next code otherwise it go to the except
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists() # if this is true to goes to if block
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)#this will retuen all cart items that are already assigned to the cart id
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation)) #by default it is query set thats why converting into list
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6] it can be like this format
+                    # ex_var_list = [4, 6, 3, 5]
+                  
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr) #it gives postion of common item
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+            except:
+                pass    
             auth.login(request,user)
             messages.success(request,'You are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER') #it grabs the previous url from where we came
+            try:
+                query = requests.utils.urlparse(url).query # this gives next=/cart/checkout/  while runing in the browser title  
+                params = dict(x.split('=') for x in query.split('&')) #it gives in key and value
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+            
         else:
             messages.error(request,'Invalid login credentials')
             return redirect('login')
     return render(request,'accounts/login.html')
+
 
 @login_required(login_url = 'login')
 def logout(request):
--- carts/context_processors.py
+++ carts/context_processors.py
@@ -8,7 +8,11 @@ def counter(request):
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            #if user is logged in then gives the count of the product based on the paticular user
+            if request.user.is_authenticated: 
+               cart_items = CartItem.objects.all().filter(user=request.user)
+            else:    
+               cart_items = CartItem.objects.all().filter(cart=cart[:1]) #if there are some many cart ids though it gives one cart id
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
         except Cart.DoesNotExist:
